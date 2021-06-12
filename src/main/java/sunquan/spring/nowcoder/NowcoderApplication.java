71d5d03b230d2068a8e09405210bf90025cedf4d
sunquan <2502665955@qq.com>
2021-06-12 23:49:31 +0800

    first commit

---
+++ src/main/java/sunquan/spring/nowcoder/NowcoderApplication.java
@@ -0,0 +1,13 @@
+package sunquan.spring.nowcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowcoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowcoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sunquan/spring/nowcoder/NowcoderApplicationTests.java
@@ -0,0 +1,13 @@
+package sunquan.spring.nowcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowcoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
